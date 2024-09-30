60f2a5ba8b543b203f7496b3a016e544e47e1108
Zion <zionperez@gmail.com>
2024-09-30 02:06:39 -0700

    Add flake files and hardware config

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  # Reference: https://nixos-and-flakes.thiscute.world/nixos-with-flakes/nixos-with-flakes-enabled
+  description = "A simple NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.user01 = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        # Import the previous configuration.nix we used,
+        # so the old configuration file still takes effect
+        ./configuration.nix
+      ];
+    };
+  };
+}
