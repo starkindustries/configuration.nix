285cc829bd10ae1ec7dce494a6dfffbc4faf359a
Zion <zionperez@gmail.com>
2024-09-29 19:45:30 -0700

    enable bluetooth

--- configuration.nix
+++ configuration.nix
@@ -58,6 +58,9 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
+  # Enable bluetooth
+  hardware.bluetooth.enable = true;
+
   # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
